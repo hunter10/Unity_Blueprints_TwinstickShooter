8233019724625f4801f513aedd9722e43fb674a2
insung jung <hunter10@naver.com>
2017-12-04 23:45:15 +0900

    First commit

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float playerSpeed = 4.0f;
+    private float currentSpeed = 0.0f;
+    private Vector3 lastMovement = new Vector3();
+	
+	void Update () {
+        Rotation();
+        Movement();
+	}
+
+    void Rotation()
+    {
+        Vector3 worldPos = Input.mousePosition;
+        worldPos = Camera.main.ScreenToWorldPoint(worldPos);
+
+        float dx = this.transform.position.x - worldPos.x;
+        float dy = this.transform.position.y - worldPos.y;
+
+        float angle = Mathf.Atan2(dy, dx) * Mathf.Rad2Deg;
+
+        Quaternion rot = Quaternion.Euler(new Vector3(0, 0, angle + 90));
+
+        this.transform.rotation = rot;
+    }
+
+    void Movement()
+    {
+        Vector3 movement = new Vector3();
+
+        movement.x += Input.GetAxis("Horizontal");
+        movement.y += Input.GetAxis("Vertical");
+
+        movement.Normalize();
+
+        if(movement.magnitude > 0)
+        {
+            currentSpeed = playerSpeed;
+            this.transform.Translate(movement * Time.deltaTime * playerSpeed, Space.World);
+            lastMovement = movement;
+        }else{
+            this.transform.Translate(lastMovement * Time.deltaTime * currentSpeed, Space.World);
+            currentSpeed *= .9f;
+        }
+    }
+}
